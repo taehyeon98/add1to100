b722ec93cf553ac289ed34d6be5455f928cf039e
hyeontae12 <thkim980416@gmail.com>
2025-09-22 09:39:52 +0900

    3의배수 출력수정
    나눗셈을 지우고 증감식을 바꿔줌

--- add1to100.cpp
+++ add1to100.cpp
@@ -8,14 +8,19 @@ int main()
     int SumMulThree = 0;
     for (int i = 1; i <= 100; i++)
     {
-        Sum = Sum + i;
+        Sum = Sum + i;/*
         if (i % 3 == 0)
         {
             SumMulThree = SumMulThree + i;
-        }
+        }*/
     }
-    cout << Sum << '\n';
-    cout << SumMulThree << '\n';
+    //*,/등이 들어가면 시간이 더 걸린다
+    for (int i = 3; i <= 100; i += 3)
+    {
+        SumMulThree += i;
+    }
+    cout << Sum << endl;
+    cout << SumMulThree << endl;
 
     return 0;
 }
