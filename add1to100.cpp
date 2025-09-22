1687637eb1ea749570d18b5c845a10ff57a2c8de
hyeontae12 <thkim980416@gmail.com>
2025-09-22 09:11:02 +0900

    1-100까지의 3의배수 합출력
    1-100까지의 3의배수 합출력

--- add1to100.cpp
+++ add1to100.cpp
@@ -5,9 +5,15 @@ using namespace std;
 int main()
 {
     int sum = 0;
+    int sumMulThree = 0;
     for (int i = 1; i <= 100; i++)
     {
         sum = sum + i;
+        if (i % 3 == 0)
+        {
+            sumMulThree = sumMulThree + i;
+        }
     }
-    cout << sum;
+    cout << sum << '\n';
+    cout << sumMulThree << '\n';
 }
